302cb8706bf294bf1b9bc907819fc80af154ac35
Martin Braun <martinbraun123@aol.com>
2016-03-26 10:57:39 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Adaption of
+
+https://emmanuelbernard.com/blog/2015/03/26/being-notified-when-commands-end
---
+++ notify.bat
@@ -0,0 +1,4 @@
+@echo off
+REM runs the notify script in the current directory
+REM to use this on your computer just change the path below
+C:\cygwin64\bin\bash -i /cygdrive/c/EASY_PATH_ACCESS/notify.sh %*
