75a69d40bae695303abe38e19df273f2bda46a29
Martin Braun <martinbraun123@aol.com>
2016-03-26 16:39:39 +0100

    Update notify.bat

--- notify.bat
+++ notify.bat
@@ -1,3 +1,9 @@
 @echo off
+REM "THE BEER-WARE LICENSE" (Revision 42):
+REM <martinbraun123@aol.com> wrote this file. As long as you retain this notice you
+REM can do whatever you want with this stuff. If we meet some day, and you think
+REM this stuff is worth it, you can buy me a beer in return Poul-Henning Kamp
+REM
+REM
 REM runs the notify script in the current directory (Windows only)
 bash -i notify.sh %*
